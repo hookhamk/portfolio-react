4a099f7926904d875ebf20d13483e95fa7edf1a2
Kelly Hookham <hookhamk8@gmail.com>
2025-02-15 17:24:56 -0600

    nav bug

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,20 @@
-// Bringing in the required import from 'react-router-dom'
 import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
-    <Navbar
-      links={[
-        <Link key={1} className="nav-link text-light" to="/">
-          Portfolio
-        </Link>,
-        <Link key={2} className="nav-link text-light" to="/about">
-          About
-        </Link>,
-        <Link key={3} className="nav-link text-light" to="/contact">
-          Contact
-        </Link>,
-        <Link key={4} className="nav-link text-light" to="/resume">
-          Resume
-        </Link>,
-      ]}
-    />
+    <nav className="navbar bg-dark">
+      <Link className="nav-link text-light" to="/">
+        Portfolio
+      </Link>
+      <Link className="nav-link text-light" to="/about">
+        About
+      </Link>
+      <Link className="nav-link text-light" to="/contact">
+        Contact
+      </Link>
+      <Link className="nav-link text-light" to="/resume">
+        Resume
+      </Link>
+    </nav>
   );
 }
