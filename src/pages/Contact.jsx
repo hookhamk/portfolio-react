eedd4b7422d6b209af7313c436bf6e156c22b0fa
Kelly Hookham <hookhamk8@gmail.com>
2025-02-15 17:50:44 -0600

    save

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,13 +3,9 @@ import {Link} from 'react-router-dom';
 export default function Contact() {
     return(
         <>
-            <h2>Contact Me</h2>
-            <div>
-            <Link to='https://github.com/hookhamk'>My Github</Link>
-            </div>
-            <div>
-            <a href="mailto:hookhamk8@gmail.com">Email Me</a>
-            </div>
+        <h2>Contact Me</h2>
+        <Link to='https://github.com/hookhamk'>My Github</Link>
+        <a href="mailto:hookhamk8@gmail.com">Email Me</a>
         </>
     )
 }
